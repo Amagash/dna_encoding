bb28f622f204ec437953dec5a121ab5349c35962
Amagash <tiffanysouterre@gmail.com>
2022-10-24 16:48:27 +0200

    add test str to bin

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,17 @@
-from src.main import dna_to_binary, binary_to_dna
+from src.main import *
 
 
 def test_dna_to_binary():
-    assert dna_to_binary("ATCG") == "00110110"   
+    assert dna_to_binary("ATCG") == "00110110"
+    assert dna_to_binary("atcg") == "00110110"
+
 
 def test_binary_to_dna():
     assert binary_to_dna("00110110") == "ATCG"
+
+
+def test_str_to_binary():
+    assert str_to_binary(
+        "HELLO WORLD !") == "01001000010001010100110001001100010011110010000001010111010011110101001001001100010001000010000000100001"
+
+
