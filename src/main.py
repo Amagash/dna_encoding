d9efa25a1005f755a7cc90fd332ca1b9ea80a173
Amagash <tiffanysouterre@gmail.com>
2022-12-12 03:58:04 +0100

    refactor main

--- src/main.py
+++ src/main.py
@@ -1,63 +1,19 @@
-def dna_to_binary(dna):
-    binary = ''
-    cipher = {
-        'A': '00',
-        'C': '01',
-        'G': '10',
-        'T': '11'
-    }
-    for base in dna.upper():
-        binary += cipher[base]
-    return binary
-
-
-def binary_to_dna(binary):
-    dna = ''
-    cipher = {
-        '00': 'A',
-        '01': 'C',
-        '10': 'G',
-        '11': 'T'
-    }
-
-    for i in range(0, len(binary), 2):
-        bits = binary[i:i+2]
-        dna += cipher[bits]
-    return dna
-
-
-def str_to_binary(sentence):
-    binary = ''
-    for char in sentence:
-        bin_char = format(ord(char), 'b')
-        if len(bin_char) < 8:
-            bin_char = ("0" * (8-len(bin_char))) + bin_char
-        binary = binary + bin_char
-    return binary
-
-
-def binary_to_str(binary):
-    sentence = ''
-    for i in range(0, len(binary), 8):
-        sentence += chr(int(binary[i:i+8], 2))
-    return sentence
-
-
-def str_to_dna(sentence):
-    binary = str_to_binary(sentence)
-    dna = binary_to_dna(binary)
-    return dna
-
-
-def dna_to_str(dna):
-    binary = dna_to_binary(dna)
-    sentence = binary_to_str(binary)
-    return sentence
+from utils.conversion import Conversion
 
 
 if __name__ == '__main__':
-    sentence = "start"
-    dna = str_to_dna(sentence)
-    print(dna)
-    result = dna_to_str(dna)
-    print(result)
+    conversion = Conversion()
+
+    dna = "CACGCTCCCGTGAGAACGCGCGACCGATCTCAAGAAATGGAGAACTATCTCCCGGCCTCACGCCAGAATGAAAGAACGCACGTTCGTGAGTAAGAACGTAAGCTCAGCCGTGCTATCTCACGGCCTCACTCCCTCAAGAACCAACGACCTATCTCACGCCCTCCCTAGAGAACTAACGTTCTATCTATTGGACGCACGCCAGAACTCCCGTGAGAACTCGCGGCCGCTCGTGCGTTCGAGCGTACGCCAGAACTACCTCCAGCTCGGCCGTAAGAACGTACGCCCTCCCTAGAGAACGCCCTATCTCAAGAACGGCCGTCCTAACGTTCTATCTATCGGCCGAGCGTACGCCAGAACGCACGCCAGAACTAGCGCCCTCGCGCCCGTGCGCACTAGCGCCAGAAAGAC"
+
+    binary = conversion.dna_to_binary(dna)
+    print("BINARY: ", binary)
+    secret = conversion.binary_to_str(binary)
+    print("SECRET: ***")
+    # print("SECRET: ", secret)
+
+    new_dna = conversion.str_to_dna(secret)
+    print("NEW DNA: ", new_dna)
+    retrieved_message = conversion.dna_to_str(new_dna)
+    print("RETRIEVED MESSAGE: ***")
+    # print("RETRIEVED MESSAGE: ", retrieved_message)
