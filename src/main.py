e3a008a91ff518b9689f6fe57accc86d1dc425c1
Amagash <tiffanysouterre@gmail.com>
2023-01-22 02:02:09 +0100

    clean main

--- src/main.py
+++ src/main.py
@@ -1,21 +1,14 @@
-from utils.conversion import Conversion
+from src.utils.conversion import Conversion
+
 
 def main():
     conversion = Conversion()
 
-    dna = "CACGCTCCCGTGAGAACGCGCGACCGATCTCAAGAAATGGAGAACTATCTCCCGGCCTCACGCCAGAATGAAAGAACGCACGTTCGTGAGTAAGAACGTAAGCTCAGCCGTGCTATCTCACGGCCTCACTCCCTCAAGAACCAACGACCTATCTCACGCCCTCCCTAGAGAACTAACGTTCTATCTATTGGACGCACGCCAGAACTCCCGTGAGAACTCGCGGCCGCTCGTGCGTTCGAGCGTACGCCAGAACTACCTCCAGCTCGGCCGTAAGAACGTACGCCCTCCCTAGAGAACGCCCTATCTCAAGAACGGCCGTCCTAACGTTCTATCTATCGGCCGAGCGTACGCCAGAACGCACGCCAGAACTAGCGCCCTCGCGCCCGTGCGCACTAGCGCCAGAAAGAC"
+    dna = "CCATCCCACAACCCAGCCCACCCACGGACGGCCTATAGAACAGCCCATAGATCGACAGCGCTATCGCCCGATCTAGCACCCTCAGGCTCGTCCGCCCCATCTATCAACCGCTCACCAGACCCATCCCACATTCCAA"
+    retrieved_message = conversion.dna_to_str(dna)
 
-    binary = conversion.dna_to_binary(dna)
-    print("BINARY: ", binary)
-    secret = conversion.binary_to_str(binary)
-    print("SECRET: ***")
-    # print("SECRET: ", secret)
-
-    new_dna = conversion.str_to_dna(secret)
-    print("NEW DNA: ", new_dna)
-    retrieved_message = conversion.dna_to_str(new_dna)
-    print("RETRIEVED MESSAGE: ***")
-    # print("RETRIEVED MESSAGE: ", retrieved_message)
+    print("The DNA sequence: " + dna) 
+    print("contains the secret message: " + retrieved_message)
 
 
 if __name__ == '__main__':
